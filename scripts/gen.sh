e86862265ed82dbaea894a50daedbd7e4816f06e
Release Standard <releasestandard@netc.eu>
2021-07-21 00:27:20 +0200

    relative implementation system and modules

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -13,3 +13,5 @@ cat "${g}/settings.modules.gradle" >> "$O/$o"
 cat "${g}/settings.lib.gradle" > "$O/$o2"
 cat "${g}/settings.common.gradle" >> "$O/$o2"
 cat "${g}/settings.root.gradle" >> "$O/$o2"
+
+cp -r "${g}/build/" "$O"
