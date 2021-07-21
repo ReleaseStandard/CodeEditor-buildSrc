30eb3a599d5cb24919aa225957c99e9586f2b5f2
Release Standard <releasestandard@netcourrier.com>
2021-07-21 16:25:42 +0000

    cleaning

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
-
+#
+# Build script for groovy scripts
+#
 
 g=src/main/groovy/
 O=outputs/
 o=settings.modules.gradle
 o2=settings.root.gradle
 
-cat "${g}/settings.lib.gradle" > "$O/$o"
-cat "${g}/settings.common.gradle" >> "$O/$o"
-cat "${g}/settings.modules.gradle" >> "$O/$o"
+VAR=""
+function configure() {
+	VAR="$1"
+	> "$VAR"
+}
+function apply() {
+	cat "$@" >> "$VAR"
+}
+function apply2() {
+	echo "$@" >> "$VAR"
+}
+function apply3() {
+	for f in "$@" ; do apply "${g}/${f}" ; done
+}
+mkdir -p "$O"
 
-cat "${g}/settings.lib.gradle" > "$O/$o2"
-cat "${g}/settings.common.gradle" >> "$O/$o2"
-cat "${g}/settings.root.gradle" >> "$O/$o2"
+configure "$O/$o"
+apply2 "// All modules (CodeEditor modules) must have the same settings in entry"
+apply2 "// this script play this role"
+apply2 ""
+apply3 settings.lib.gradle settings.common.gradle settings.modules.gradle
+
+
+configure "$O/$o2"
+apply2 "// root project of CodeEditor have common parts with modules so we manage that here"
+apply2 ""
+apply3 settings.lib.gradle settings.common.gradle settings.root.gradle
 
 cp -r "${g}/build/" "$O"
